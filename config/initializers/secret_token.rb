afc5aa2516fe516cb739aaae0c52c4cad4141d04
Gerardo Marambio <gimarambio@gmail.com>
2014-06-05 15:20:35 -0400

    app inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hola::Application.config.secret_key_base = 'ff2ccf223311692ee06d1cffbe04928d749ef69785f0e07d5bfbbe7fc80cbe085a1ce466e4d32f6e65d5acdfd70464429125a9d6ffebcd6c00614c04a93f068b'
